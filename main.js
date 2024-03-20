18fb38e509b985e38327a1b4eeed862600fb326f
Aksanti Bahiga Tacite <tacite.bahiga@gmail.com>
2024-03-20 06:34:18 +0200

    Storage API

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
 const btn = document.querySelector("button");
+const form = document.querySelector("form");
+const tableau = document.querySelector("table");
 
-btn.addEventListener("click", async () => {
-  if ("EyeDropper" in window) {
-    // The API is available!
-    const eyeDropper = new EyeDropper();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const titre = document.querySelector("#titre").value;
+  const auteur = document.querySelector("#auteur").value;
 
-    try {
-      const result = await eyeDropper.open();
-      // The user selected a pixel, here is its color:
-      const colorHexValue = result.sRGBHex;
-      const p = document.querySelector("p");
-      p.textContent = `Couleur choisie : ${colorHexValue}`;
-      p.style.color = colorHexValue;
-    } catch (err) {
-      // The user escaped the eyedropper mode.
-    }
-  }
+  // local storage
+  const book = {
+    titre,
+    auteur,
+  };
+  const books = JSON.parse(sessionStorage.getItem("books")) || [];
+  books.push(book);
+  sessionStorage.setItem("books", JSON.stringify(books));
+
+  form.reset();
+  afficherLivres();
 });
+
+const afficherLivres = () => {
+  const books = JSON.parse(localStorage.getItem("books")) || [];
+
+  const tbody = document.querySelector("#book-list");
+
+  tbody.innerHTML = "";
+
+  books.forEach((book) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${book.titre}</td>
+    <td>${book.auteur}</td>
+    <td><button class="delete">X</button></td>
+    `;
+    tbody.appendChild(row);
+
+    const deleteBtn = row.querySelector(".delete");
+    deleteBtn.addEventListener("click", () => {
+      deleteBook(book.titre);
+    });
+  });
+};
+
+afficherLivres();
+
+const deleteBook = (titre) => {
+  const books = JSON.parse(localStorage.getItem("books")) || [];
+
+  const newBooks = books.filter((book) => book.titre !== titre);
+
+  localStorage.setItem("books", JSON.stringify(newBooks));
+  afficherLivres();
+};
